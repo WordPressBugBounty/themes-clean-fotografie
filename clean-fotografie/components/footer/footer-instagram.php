d46848544440938e86eea2dee29e1e0aebe65b2e
Sean Murphy <sean@iamseanmurphy.com>
2024-10-20 02:22:08 +0000

    1.4.5

---
+++ clean-fotografie/components/footer/footer-instagram.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Displays footer instagram widget
+ *
+ * @package Christmas_Bell
+ */
+?>
+
+<?php
+if ( is_active_sidebar( 'sidebar-instagram' ) ) :
+?>
+<aside id="footer-instagram" class="widget-area" role="complementary">
+	<div class="wrapper">
+		<div class="footer-instagram">
+			<?php dynamic_sidebar( 'sidebar-instagram' ); ?>
+		</div>
+	</div><!-- .wrapper -->
+</aside><!-- .widget-area -->
+<?php endif;
+
